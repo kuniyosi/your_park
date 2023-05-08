05143eb8dfbfdc57286bff101d2920cc64527c07
nakazawa <zhongzejiangji@gmail.com>
2023-05-08 03:24:27 +0000

    device add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope module: :public do
+  
+    devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+    }
+  
+    root to: 'homes#top'
+    get '/about', to: 'homes#about'
+  
+  end
+  
+  namespace :admin do
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+  
+  get '', to: 'homes#top'
+  
+  end
+ 
+  
 end
