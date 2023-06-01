c0eee6ec8d763a578d5f5262a8072f9f34fe740e
nakazawa <zhongzejiangji@gmail.com>
2023-06-01 06:02:47 +0000

    layout image edit

--- app/models/park.rb
+++ app/models/park.rb
@@ -17,12 +17,12 @@ class Park < ApplicationRecord
     Park.where(['area LIKE ?', "#{search_word}"])
   end
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   def favorited_by?(customer)
