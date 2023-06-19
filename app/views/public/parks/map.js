75b2742850e0f232c673aa0f57a72fdc66a327d4
nakazawa <zhongzejiangji@gmail.com>
2023-06-19 14:04:29 +0000

    map.js delete

--- app/views/public/parks/map.js
+++
@@ -1,6 +0,0 @@
-function initMap(){
-    let map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: <%= @park.latitude %>, lng: <%= @post.longitude %> },
-    zoom: 15
-    });
-}
